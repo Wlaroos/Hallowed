7009e2b8e3d6b1d83c4bcfc90c4bc2e6a9c668f3
Mikey <mikeybess27@gmail.com>
2025-10-04 21:28:57 -0500

    Added player grid movement, walls and camera

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var tile_size = 12
+
+var inputs = {
+	"MoveRight": Vector2.RIGHT,
+	"MoveLeft": Vector2.LEFT,
+	"MoveUp": Vector2.UP,
+	"MoveDown": Vector2.DOWN }
+	
+@onready var ray = $RayCast2D
+	
+func _ready() -> void:
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event: InputEvent) -> void:
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += inputs[dir] * tile_size
